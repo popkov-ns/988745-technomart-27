7b93247ab68517792293975b337c5143bbc7c6cf
Nikita Popkov <popkov-ns@mail.ru>
2020-03-20 00:06:19 +0300

    Добавил фильтр на страницу каталог

--- js/buy.js
+++ js/buy.js
@@ -1,8 +1,23 @@
+// Modal buy
+
 let buttonModalBuy = document.querySelectorAll('.button-buy');
 let popupModalBuy = document.querySelector('.modal-buy');
 let closeModalBuy = popupModalBuy.querySelector('.button-close');
+let сontinueModalBuy = popupModalBuy.querySelector('.continue-buy');
 
-buttonModalBuy.onClickButton('click', function (evt) {
+for (let i = 0; i < buttonModalBuy.length; i++) {
+  buttonModalBuy[i].addEventListener("click", function(evt) {
     evt.preventDefault();
-    popupModalBuy.classList.add('modal-show');
+    popupModalBuy.classList.add("modal-show");
+  });
+};
+
+closeModalBuy.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popupModalBuy.classList.remove('modal-show');
+});
+
+сontinueModalBuy.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popupModalBuy.classList.remove('modal-show');
 });
